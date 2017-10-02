0ddff6888108dca4622124fbaaec65ed3ca44f1d
saltyJeff <saltyJeff@users.noreply.github.com>
2017-10-02 08:59:37 -0700

    adding zelda

---
+++ src/io/github/saltyJeff/musiclist/songs/Zelda.java
@@ -0,0 +1,13 @@
+package io.github.saltyJeff.musiclist.songs;
+
+import io.github.saltyJeff.musiclist.Note;
+import io.github.saltyJeff.musiclist.Sequence;
+
+public class Zelda {
+	public static Sequence getSong() {
+		double beat = 0.5;
+		Sequence seq = new Sequence();
+		
+		return seq;
+	}
+}
